b8c7d0ee5545a7d594f51dc183d0bdf71527d330
yancy <dongyang@wondertek.com.cn>
2022-12-05 18:32:32 +0800

    add constant info

--- classreader/class_file.py
+++ classreader/class_file.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import logging
 from typing import NewType
 
@@ -15,8 +16,164 @@ class AttributeInfo:
   pass
 
 
+class ConstantInfo:
+  def __init__(self, tag: int, cp: ConstantPool) -> None:
+    self.tag = tag
+    self.cp = cp
+
+  def read_info(self, reader: ClassReader) -> None:
+    raise Exception("not implemented!")
+
+
 class ConstantPool:
+  def __init__(self) -> None:
+    self.infos: list[ConstantInfo] = [None]
+
+  def append(self, info: ConstantInfo) -> None:
+    self.infos.append(info)
+
+  def get_constant_info(index: uint16) -> ConstantInfo:
+    pass
+
+  def get_name_and_type(index: uint16) -> tuple[str, str]:
+    pass
+
+  def get_class_name(index: uint16) -> str:
+    pass
+
+  def get_utf8(index: uint16) -> str:
+    pass
+
+
+CONSTANT_CLASS = 7
+CONSTANT_FIELDREF = 9
+CONSTANT_METHODREF = 10
+CONSTANT_INTERFACE_METHODREF = 11
+CONSTANT_STRING = 8
+CONSTANT_INTEGER = 3
+CONSTANT_FLOAT = 4
+CONSTANT_LONG = 5
+CONSTANT_DOUBLE = 6
+CONSTANT_NAME_AND_TYPE = 12
+CONSTANT_UTF8 = 1
+CONSTANT_METHOD_HANDLE = 15
+CONSTANT_METHOD_TYPE = 16
+CONSTANT_INVOKE_DYNAMIC = 18
+
+
+class DoublejConstantInfo(ConstantInfo):
+  def __init__(self, tag: int, cp: ConstantPool) -> None:
+    super().__init__(tag, cp)
+    self.value: float
+
+  def read_info(self, reader: ClassReader) -> None:
+    import struct
+    data = reader.read_bytes(8)
+    [self.value] = struct.unpack('d', data)
+
+
+class FloatConstantInfo(ConstantInfo):
+  def __init__(self, tag: int, cp: ConstantPool) -> None:
+    super().__init__(tag, cp)
+    self.value: float
+
+  def read_info(self, reader: ClassReader) -> None:
+    import struct
+    data = reader.read_bytes(4)
+    [self.value] = struct.unpack('f', data)
+
+
+class LongConstantInfo(ConstantInfo):
+  def __init__(self, tag: int, cp: ConstantPool) -> None:
+    super().__init__(tag, cp)
+    self.value: int
+
+  def read_info(self, reader: ClassReader) -> None:
+    self.value = reader.read_u64()
+
+
+class IntegerConstantInfo(ConstantInfo):
+  def __init__(self, tag: int, cp: ConstantPool) -> None:
+    super().__init__(tag, cp)
+    self.value: int
+
+  def read_info(self, reader: ClassReader) -> None:
+    self.value = reader.read_u32()
+
+
+class StringConstantInfo(ConstantInfo):
+  def __init__(self, tag: int, cp: ConstantPool) -> None:
+    super().__init__(tag, cp)
+    self.string_index: uint16
+
+  def read_info(self, reader: ClassReader) -> None:
+    self.string_index = reader.read_u16()
+
+
+class CommonConstantInfo(ConstantInfo):
+  def __init__(self, tag: int, cp: ConstantPool) -> None:
+    super().__init__(tag, cp)
+    self.class_index: uint16
+    self.name_and_type_index: uint16
+
+  def read_info(self, reader: ClassReader) -> None:
+    self.class_index = reader.read_u16()
+    self.name_and_type_index = reader.read_u16()
+
+
+class InterfaceMethodrefConstantInfo(ConstantInfo):
+  def __init__(self, tag: int, cp: ConstantPool) -> None:
+    super().__init__(tag, cp)
+    self.info: ConstantInfo = CommonConstantInfo(tag, cp)
+
+  def read_info(self, reader: ClassReader) -> None:
+    self.info.read_info(reader)
+
+
+class MethodrefConstantInfo(ConstantInfo):
+  def __init__(self, tag: int, cp: ConstantPool) -> None:
+    super().__init__(tag, cp)
+    self.info: ConstantInfo = CommonConstantInfo(tag, cp)
+
+  def read_info(self, reader: ClassReader) -> None:
+    self.info.read_info(reader)
+
+
+class FieldrefConstantInfo(ConstantInfo):
+  def __init__(self, tag: int, cp: ConstantPool) -> None:
+    super().__init__(tag, cp)
+    self.info: ConstantInfo
+
+  def read_info(self, reader: ClassReader) -> None:
+    self.info.read_info(reader)
+
+
+class ClassConstantInfo(ConstantInfo):
+  def __init__(self, tag: int, cp: ConstantPool) -> None:
+    super().__init__(tag, cp)
+    self.name_index: uint16
+
+  def read_info(self, reader: ClassReader) -> None:
+    self.name_index = reader.read_u16()
+
+
+def get_constant_info(tag: int, cp: ConstantPool) -> ConstantInfo:
+  info: ConstantInfo
+  if tag == CONSTANT_CLASS:
+    info = ClassConstantInfo(tag, cp)
+
+
+def read_constant_info(reader: ClassReader, cp: ConstantPool) -> ConstantInfo:
+  cp = reader.read_u8()
   pass
+
+
+def read_constant_pool(reader: ClassReader) -> ConstantPool:
+  counts = reader.read_u16()
+  cp = ConstantPool()
+  for i in range(1, counts):
+    cp.append(read_constant_info(reader))
+    pass
 
 
 class MemberInfo:
@@ -26,18 +183,38 @@ class MemberInfo:
     self.name_index: uint16
     self.descriptor_index: uint16
     self.attributes: list[AttributeInfo]
-  pass
+
+  def name(self) -> str:
+    return self.cp.get_utf8(self.name_index)
+
+  def descriptor(self) -> str:
+    return self.cp.get_utf8(self.descriptor_index)
 
 
-def read_constant_pool(reader: ClassReader) -> ConstantPool:
-  pass
+def read_members(reader: ClassReader, cp: ConstantPool) -> list[MemberInfo]:
+  counts = reader.read_u16()
+  members = [read_member(reader, cp) for i in range(counts)]
+  # for i in range(counts):
+  #     members[i] = read_member(reader, cp)
+  return members
+
+
+def read_member(reader: ClassReader, cp: ConstantPool) -> MemberInfo:
+  info = MemberInfo()
+  info.cp = cp
+  info.access_flags = reader.read_u16()
+  info.name_index = reader.read_u16()
+  info.descriptor_index = reader.read_u16()
+  info.attributes = read_attributes(reader, cp)
 
 
 def read_members(reader: ClassReader, pool: ConstantPool) -> list[MemberInfo]:
   pass
 
+
 def read_member(reader: ClassReader, pool: ConstantPool) -> MemberInfo:
   pass
+
 
 def read_attributes(reader: ClassReader, pool: ConstantPool) -> list[AttributeInfo]:
   pass
@@ -73,11 +250,10 @@ class ClassFile:
 
   # check magic number
   def read_and_check_magic(self, reader: ClassReader):
-    magic = reader.read_u32()
-    if magic != 0xCAFEBABE:
+    self.magic = reader.read_u32()
+    if self.magic != 0xCAFEBABE:
       raise Err(
-          f"java.lang.ClassFormatError: expected magic number 0xCAFEBABE, got {magic:0x}")
-    self.magic = magic
+          f"java.lang.ClassFormatError: expected magic number 0xCAFEBABE, got {self.magic:0x}")
 
   # read and check class file version
   def read_and_check_version(self, reader: ClassReader):
